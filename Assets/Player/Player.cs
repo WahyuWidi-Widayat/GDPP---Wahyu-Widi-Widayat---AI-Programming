55da4780674b2ec7e5d3a8c28dec7a68ab049200
WahyuWidiWidayat <widayat.wahyuwidi28@gmail.com>
2025-09-05 13:54:23 +0700

    ThirdPersonCamera
    Mmebuat camera third person dengan CineMachine,Melakukan lock kursor

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody _rigidBody;
-    [SerializeField] private float _speed; 
+    [SerializeField] private float _speed = 5f;
+
+    // Tambahan: drag Main Camera ke sini lewat Inspector
+    [SerializeField] private Transform _cameraTransform;
 
     private void Awake()
     {
-        _rigidBody = GetComponent<Rigidbody>(); 
+        _rigidBody = GetComponent<Rigidbody>();
+        HideAndLockCursor();
     }
 
+    private void HideAndLockCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
+        // arah gerakan relatif kamera
+        Vector3 horizontalDirection = horizontal * _cameraTransform.right;
+        Vector3 verticalDirection = vertical * _cameraTransform.forward;
 
-       
+        // buang komponen Y biar tidak miring
+        horizontalDirection.y = 0;
+        verticalDirection.y = 0;
+
+        // gabungkan
+        Vector3 movementDirection = (horizontalDirection + verticalDirection).normalized;
+
+        // apply ke rigidbody
         _rigidBody.linearVelocity = movementDirection * _speed;
     }
 }
